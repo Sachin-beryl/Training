5c826c5eccae56c0d39193a6478760cddc03d6e8
Sachin-beryl <sachin.kanojia@berylsystems.com>
2023-02-23 15:52:50 +0530

    implementation of modules

---
+++ Ruby/ruby_code_practise/Module_tool.rb
@@ -0,0 +1,9 @@
+module Module_tool
+    def hlo(name)
+        puts "hlo #{name}"
+    end
+    def bye(name)
+        puts "bye #{name}"
+    end
+end
+    
---
+++ Ruby/ruby_code_practise/module_test.rb
@@ -0,0 +1,4 @@
+require_relative "Module_tool.rb"     #relative is used to find module in same folder
+include Module_tool
+
+Module_tool.hlo('ram')       #calling the module method .
