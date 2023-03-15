d441b1b30f33ece8b4306a76db621ae0a730f32d
Sachin-beryl <sachin.kanojia@berylsystems.com>
2023-03-15 18:08:02 +0530

    Database migration in blog project on rails

--- Rails/Projects/blog/db/migrate/20230315120441_create_articles.rb
+++ Rails/Projects/blog/db/migrate/20230315120441_create_articles.rb
@@ -1,10 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
-      t.string :title
+      t.string :title       #created during model generation.
       t.text :body
 
-      t.timestamps
+      t.timestamps      #add column created_at and updated_at and managed by rails
     end
   end
 end
