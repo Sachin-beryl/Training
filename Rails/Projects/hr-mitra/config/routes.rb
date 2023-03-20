921ca0d72402cdcb6193522ace94a371b5887a3d
Sachin-beryl <sachin.kanojia@berylsystems.com>
2023-03-20 11:15:50 +0530

    Creating controller using Rails generator

--- Rails/Projects/hr-mitra/config/routes.rb
+++ Rails/Projects/hr-mitra/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
+  get 'about' => 'pages#about_us'
+  get 'contact' => 'pages#contact_us'
+  get 'privacy-policy' => 'pages#privacy_policy'
+  get 'terms-and-conditions' => 'pages#terms_and_conditions'
 end
