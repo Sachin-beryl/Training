5af30d806cce9f6acf6cefc367ef07c3e29db629
Sachin-beryl <sachin.kanojia@berylsystems.com>
2023-03-21 16:43:23 +0530

    Deleting Comments in blog project

--- Rails/Projects/blog/app/models/article.rb
+++ Rails/Projects/blog/app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include Visible
-  
-  has_many :comments
+
+  has_many :comments, dependent: :destroy   #article associated comments will also need to be deleted.
 
   validates :title, presence: true   
   validates :body, presence: true, length: { minimum: 10 } 
