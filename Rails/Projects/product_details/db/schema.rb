071f70c91c6429536f8f64dc7fc2122f47e1072e
Sachin-beryl <sachin.kanojia@berylsystems.com>
2023-03-23 14:13:50 +0530

    create user model,reference in user and product , join both tables

--- Rails/Projects/product_details/db/schema.rb
+++ Rails/Projects/product_details/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_065030) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_072721) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_065030) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "price"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
+  create_table "products_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.index ["product_id", "user_id"], name: "index_products_users_on_product_id_and_user_id"
+    t.index ["user_id", "product_id"], name: "index_products_users_on_user_id_and_product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "gender"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "products", "users"
 end
