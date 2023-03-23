08fcdebb20762781dc7113bca23a64bbc5ac6744
Sachin-beryl <sachin.kanojia@berylsystems.com>
2023-03-23 15:06:21 +0530

    Write a Migration (create a table and join table) manual

--- Rails/Projects/product_details/db/schema.rb
+++ Rails/Projects/product_details/db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_072721) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_092843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categorization", id: false, force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_categorization_on_category_id"
+    t.index ["product_id"], name: "index_categorization_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
