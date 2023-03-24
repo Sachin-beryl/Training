eef8a5c1f764109f4ee0e62ff62b76c34fc9b35e
Sachin-beryl <sachin.kanojia@berylsystems.com>
2023-03-24 15:46:34 +0530

    Schema dump and change primary key name in migration

--- Rails/Projects/product_details/db/schema.rb
+++ Rails/Projects/product_details/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_064804) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_100749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -32,9 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_064804) do
 
   create_table "manager", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "productdetails", force: :cascade do |t|
@@ -56,6 +55,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_064804) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "seller", primary_key: "s_id", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
