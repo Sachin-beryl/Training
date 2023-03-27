8e9d78e5abef88ab7da605bcd32529ae6b871a1c
Sachin-beryl <sachin.kanojia@berylsystems.com>
2023-03-27 15:08:15 +0530

    comparison,exclusion,format,inclusion in validation (model)

--- Rails/Projects/form/app/models/form_detail.rb
+++ Rails/Projects/form/app/models/form_detail.rb
@@ -1,10 +1,12 @@
 class FormDetail < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }
   validates :gender, presence: true
   validates :age, presence: true
-  validates :address, presence: true
+  validates :address, presence: true, exclusion: { in: %w(delhi up mp),message: "%{value} is invalid ." }
   validates :terms_and_conditions, acceptance: { message: 'must be accepted' }
   validates :relocate, acceptance: { accept: ['yes','no'] }, presence: true
   validates :email, confirmation: true, presence: true
   validates :email_confirmation, presence: true
+  validates :form_id, comparison: { greater_than_or_equal_to: 1000 }, presence:true 
+  validates :identity, inclusion: { in: %w(aadhar_card pan_card driving_license),message: "%{value} is invalid"}
 end
--- Rails/Projects/form/db/schema.rb
+++ Rails/Projects/form/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_065828) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_093030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_065828) do
     t.datetime "updated_at", null: false
     t.string "relocate"
     t.string "email"
+    t.string "identity"
+    t.integer "form_id"
   end
 
 end
