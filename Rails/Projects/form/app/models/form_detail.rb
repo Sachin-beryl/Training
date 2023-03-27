48125a683252a15f5a5ab6aee3e139ce289c6159
Sachin-beryl <sachin.kanojia@berylsystems.com>
2023-03-27 18:20:30 +0530

    Common Validation Options in models

--- Rails/Projects/form/app/models/form_detail.rb
+++ Rails/Projects/form/app/models/form_detail.rb
@@ -12,15 +12,16 @@ class FormDetail < ApplicationRecord
   end
 
   validates_with NameValidator     #calling created validation
-  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }
-  validates :gender, presence: true
+  validates :name, presence: { message: "must be given please" }, format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }
+  validates :gender, presence: { message: "must be given please" }
   validates :age, presence: true, numericality: {greater_than_or_equal_to: 30, even: true }
   validates :address, presence: true, exclusion: { in: %w(delhi up mp),message: "%{value} is invalid ." }
-  validates :terms_and_conditions, acceptance: { message: 'must be accepted' }
+  validates :terms_and_conditions, acceptance: { message: 'must be accepted' } 
   validates :relocate, acceptance: { accept: ['yes','no'] }, presence: true
-  validates :email, confirmation: true, presence: true, uniqueness: true
-  validates :email_confirmation, presence: true
+  validates :email, confirmation: true, presence: true, uniqueness: true, on: :create
+  validates :email_confirmation, presence: { strict: true }  #must be present
   validates :form_id, comparison: { greater_than_or_equal_to: 1000 }, presence:true , length: { minimum: 4, maximum:5 }, numericality: { only_integer: true }
-  validates :identity, inclusion: { in: %w(aadhar_card pan_card driving_license),message: "%{value} is invalid"}
+  validates :identity, inclusion: { in: %w(aadhar_card pan_card driving_license),message: "%{value} is invalid"},  allow_blank: true
   validates :mobile, presence: true
+  validates :salary, numericality: { only_integer: true }, allow_nil: true
 end
