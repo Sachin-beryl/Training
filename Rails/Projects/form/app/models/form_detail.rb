eadf91ba3b9a1e57c449da6d211208279c4bcdf5
Sachin-beryl <sachin.kanojia@berylsystems.com>
2023-03-27 17:27:57 +0530

    Validates_with and validates_each in validation (model)

--- Rails/Projects/form/app/models/form_detail.rb
+++ Rails/Projects/form/app/models/form_detail.rb
@@ -1,4 +1,17 @@
 class FormDetail < ApplicationRecord
+  class NameValidator < ActiveModel::Validator     #create validation
+    def validate(record)
+      if record.name == "neeraj"
+        record.errors.add :name, "can't be neeraj"
+      end
+    end
+  end
+
+  validates_each :name do |record, attr, value|   #check for name start with capital letter.
+    record.errors.add(attr, 'must start with upper case') if value =~ /\A[[:lower:]]/
+  end
+
+  validates_with NameValidator     #calling created validation
   validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }
   validates :gender, presence: true
   validates :age, presence: true, numericality: {greater_than_or_equal_to: 30, even: true }
@@ -9,5 +22,5 @@ class FormDetail < ApplicationRecord
   validates :email_confirmation, presence: true
   validates :form_id, comparison: { greater_than_or_equal_to: 1000 }, presence:true , length: { minimum: 4, maximum:5 }, numericality: { only_integer: true }
   validates :identity, inclusion: { in: %w(aadhar_card pan_card driving_license),message: "%{value} is invalid"}
-  validates :mobile, absence: true
+  validates :mobile, presence: true
 end
