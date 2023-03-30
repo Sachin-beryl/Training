e4588d26e23064d57f87d9db761b6ed8ba3236e5
Sachin-beryl <sachin.kanojia@berylsystems.com>
2023-03-30 17:43:48 +0530

    has_many :through Association in rails

--- Rails/Projects/library/app/models/book.rb
+++ Rails/Projects/library/app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   has_many :authors
   has_one :publisher
+
+  # for has_one :through: 
+  has_many :book_categories
+  has_many :categories, through: :book_categories
 end
--- Rails/Projects/library/db/schema.rb
+++ Rails/Projects/library/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_30_100900) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_112147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,9 +22,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_30_100900) do
     t.index ["book_id"], name: "index_authors_on_book_id"
   end
 
+  create_table "book_categories", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_book_categories_on_book_id"
+    t.index ["category_id"], name: "index_book_categories_on_category_id"
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.string "book_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,5 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_30_100900) do
   end
 
   add_foreign_key "authors", "books"
+  add_foreign_key "book_categories", "books"
+  add_foreign_key "book_categories", "categories"
   add_foreign_key "publishers", "books"
 end
